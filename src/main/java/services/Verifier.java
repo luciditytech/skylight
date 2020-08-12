55b6ab4280f726dead928caa3233417a84c8aa00
Miguel Morales <therevoltingx@gmail.com>
2020-08-12 03:08:35 -0700

    chore(*): misc cleanups

--- src/main/java/services/Verifier.java
+++ src/main/java/services/Verifier.java
@@ -14,6 +14,7 @@ import java.util.Base64;
 import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
 import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.util.encoders.Hex;
 
 public class Verifier {
   static {
@@ -30,6 +31,7 @@ public class Verifier {
     PublicKey publicKey = new JcaPEMKeyConverter().setProvider("BC").getPublicKey(keyInfo);
 
     byte[] decoded = Base64.getDecoder().decode(signature);
+    // byte[] decoded = Hex.decode(signature);
 
     Signature signer = Signature.getInstance("ECDSA", "BC");
     signer.initVerify(publicKey);
